cc762f6fcf13c6eabb15b84c681314177ad34e38
Olga Kozlova <for.confirmations@gmail.com>
2021-08-09 19:25:29 +0300

    Патч для относительных адресов

--- static/js/ghpages.js
+++ static/js/ghpages.js
@@ -24,10 +24,38 @@
     });
   }
 
+  // подправляет адреса/ссылки в HTML, если вдруг сайт находится не в корне домена
+  // (some-user.github.io/ovdinfo вместо ovdinfo.github.io)
+  function patchSiteRoot() {
+    var rootUrl = new URL(window.location);
+    if (rootUrl.hostname === 'localhost') return;
+
+    function getPatchedURL(urlStr) {
+      var url = new URL(urlStr);
+      if (url.origin !== rootUrl.origin) return url;
+      if (!/ovdinfo/.test(url.host) && !/^\/ovdinfo/.test(url.pathname)) {
+        url.pathname = '/ovdinfo' + url.pathname;
+      }
+      return url;
+    }
+    document.querySelectorAll('a').forEach(function(link) {
+      link.href = getPatchedURL(link.href);
+    });
+    document.querySelectorAll('img').forEach(function(img) {
+      img.src = getPatchedURL(img.src);
+    });
+    document.querySelectorAll('[style]').forEach(function(node) {
+      var bgImg = getComputedStyle(node).backgroundImage.replace('url("', '').replace('")', '');
+      if (bgImg === 'none') return;
+      node.style.backgroundImage = 'url("' + getPatchedURL(bgImg) + '")';
+    });
+  }
+
   function onReady() {
     if (document.readyState === 'interactive' || document.readyState === 'complete') {
       document.removeEventListener('readystatechange', onReady);
       buildMobileLayout();
+      patchSiteRoot();
     } else {
       document.addEventListener('readystatechange', onReady);
     }
